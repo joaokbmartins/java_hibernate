c90c070d390a5018ce9c61dc009522fcaed37e40
joaokbmartins <martins.joao1909@gmail.com>
2021-08-01 21:14:52 -0300

    fix: engine updated to innodb

--- src/main/java/relations/oneToMany/Account.java
+++ src/main/java/relations/oneToMany/Account.java
@@ -3,7 +3,6 @@ package relations.oneToMany;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -14,13 +13,12 @@ public class Account {
 	@Id
 	@Column(name = "id")
 	private int id;
-	@Column(name = "number", unique = true)
+	@Column(name = "number", unique = true, nullable = false)
 	private int number;
 	@Column(name = "owner")
 	private String owner;
 
-	@ManyToOne
-	@JoinColumn(name = "bank_id", referencedColumnName = "id")
+	@ManyToOne(targetEntity = Bank.class, optional = false)
 	private Bank bank;
 
 	public Bank getBank() {
