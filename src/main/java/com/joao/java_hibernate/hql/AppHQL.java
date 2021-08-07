272b8243c11c46c96af503d539e450ee4ee5cb95
joaokbmartins <martins.joao1909@gmail.com>
2021-08-06 23:51:35 -0300

    feat: Hibernate NativeQuery

--- src/main/java/com/joao/java_hibernate/hql/AppHQL.java
+++ src/main/java/com/joao/java_hibernate/hql/AppHQL.java
@@ -1,10 +1,13 @@
 package com.joao.java_hibernate.hql;
 
 import java.util.List;
+import java.util.Map;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.NativeQuery;
 import org.hibernate.query.Query;
 
 import com.joao.java_hibernate.relations.oneToOne.Book;
@@ -15,14 +18,14 @@ public class AppHQL {
 
 		Configuration config = new Configuration().addAnnotatedClass(Book.class).configure();
 		SessionFactory sf = config.buildSessionFactory();
-		Session s1 = sf.openSession();
+		Session session = sf.openSession();
 
-		s1.getTransaction().begin();
+		session.getTransaction().begin();
 //		DataLoader.loadShipsPack(s1);
 
 		System.out.println("==================================================");
 
-		Query<Book> q1 = s1.createQuery("FROM Book");
+		Query<Book> q1 = session.createQuery("FROM Book");
 		List<Book> books = q1.getResultList();
 
 		for (Book b : books) {
@@ -31,7 +34,7 @@ public class AppHQL {
 
 		System.out.println("==================================================");
 
-		Query q2 = s1.createQuery("FROM Book WHERE id > 7 and id < 12");
+		Query q2 = session.createQuery("FROM Book WHERE id > 7 and id < 12");
 		List<Book> books2 = q2.getResultList();
 
 		for (Book b : books2) {
@@ -40,7 +43,7 @@ public class AppHQL {
 
 		System.out.println("==================================================");
 
-		Query q3 = s1.createQuery("SELECT author, name FROM Book WHERE id = 39");
+		Query q3 = session.createQuery("SELECT author, name FROM Book WHERE id = 39");
 		System.out.println(q3.uniqueResult());
 		Object[] b = (Object[]) q3.uniqueResult();
 
@@ -50,7 +53,7 @@ public class AppHQL {
 
 		System.out.println("==================================================");
 
-		Query q4 = s1.createQuery("SELECT id, name FROM Book WHERE id < 5");
+		Query q4 = session.createQuery("SELECT id, name FROM Book WHERE id < 5");
 		List<Object[]> info = (List<Object[]>) q4.getResultList();
 
 		for (Object[] data : info) {
@@ -59,7 +62,7 @@ public class AppHQL {
 
 		System.out.println("==================================================");
 
-		Query q5 = s1.createQuery("SELECT id, author FROM Book b WHERE b.id > 45");
+		Query q5 = session.createQuery("SELECT id, author FROM Book b WHERE b.id > 45");
 		List<Object[]> bookList = (List<Object[]>) q5.list();
 
 		for (Object[] o : bookList) {
@@ -68,28 +71,52 @@ public class AppHQL {
 
 		System.out.println("==================================================");
 
-		Query q6 = s1.createQuery("SELECT SUM(id) FROM Book");
+		Query q6 = session.createQuery("SELECT SUM(id) FROM Book");
 		Long idSum = (Long) q6.uniqueResult();
 		System.out.println(idSum);
 
 		System.out.println("==================================================");
 
-		Query q7 = s1.createQuery("SELECT AVG(id) FROM Book");
+		Query q7 = session.createQuery("SELECT AVG(id) FROM Book");
 		Double avrgId = (Double) q7.uniqueResult();
 		System.out.println(avrgId);
 
 		System.out.println("==================================================");
 
 		int param = 30;
-		Query q8 = s1.createQuery("SELECT MAX(id) FROM Book WHERE id < :limit");
+		Query q8 = session.createQuery("SELECT MAX(id) FROM Book WHERE id < :limit");
 		q8.setParameter("limit", param);
 		Integer maxId = (Integer) q8.uniqueResult();
 		System.out.println(maxId);
 
 		System.out.println("==================================================");
 
-		s1.getTransaction().commit();
-		s1.close();
+		NativeQuery<Book> q9 = session.createNativeQuery("SELECT * FROM books WHERE id > 40");
+		q9.addEntity(Book.class);
+		List<Book> sqlBooks = (List<Book>) q9.list();
+
+		for (Book book : sqlBooks) {
+			System.out.println(book);
+		}
+
+		System.out.println("==================================================");
+
+		int min = 30, max = 35;
+		NativeQuery q0 = session.createNativeQuery("SELECT author, name FROM books WHERE id > :min AND id < :max");
+		q0.setResultTransformer(Criteria.ALIAS_TO_ENTITY_MAP);
+		q0.setParameter("min", min);
+		q0.setParameter("max", max);
+		List inf = q0.list();
+
+		for (Object o : inf) {
+			Map map = (Map) o;
+			System.out.println(map.get("author") + " " + map.get("name"));
+		}
+
+		System.out.println("==================================================");
+
+		session.getTransaction().commit();
+		session.close();
 
 	}
 
