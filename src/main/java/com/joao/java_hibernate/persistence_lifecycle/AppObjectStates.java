86c92c41050089253a5bd5aea4984a418c119893
joaokbmartins <martins.joao1909@gmail.com>
2021-08-07 14:17:12 -0300

    feat: hibernate persistence lifecycle

---
+++ src/main/java/com/joao/java_hibernate/persistence_lifecycle/AppObjectStates.java
@@ -0,0 +1,55 @@
+package com.joao.java_hibernate.persistence_lifecycle;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import com.joao.java_hibernate.relations.oneToOne.Book;
+
+public class AppObjectStates {
+
+	public static void main(String[] args) {
+
+		Configuration config = new Configuration().addAnnotatedClass(Book.class).configure();
+		SessionFactory sf = config.buildSessionFactory();
+		Session session = sf.openSession();
+
+		session.getTransaction().begin();
+
+//		DataLoader.loadShipsPack(session);
+
+		int id = 56;
+
+		/**
+		 * Transient State: object not related to database:
+		 */
+		Book b1 = new Book();
+		b1.setId(id);
+		b1.setAuthor("Assis");
+		b1.setName("Dom Casmurro");
+
+		/**
+		 * Persistence State: object present in database:
+		 */
+		session.save(b1);
+
+		Query<Book> query = session.createQuery("FROM Book WHERE id = :id");
+		query.setParameter("id", id);
+		Book b2 = (Book) query.uniqueResult();
+
+		session.getTransaction().commit();
+
+		/**
+		 * Detached State: object not related to database after transaction has been
+		 * committed:
+		 */
+		b2.setAuthor("Graciliano");
+
+		System.out.println(b2);
+
+		session.close();
+
+	}
+
+}
